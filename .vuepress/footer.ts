e83657e656960895851d4eecc40b9e3ef088788d
huli1314256 <154992364+huli1314256@users.noreply.github.com>
2024-01-12 15:45:39 +0800

    Add files via upload

---
+++ .vuepress/footer.ts
@@ -0,0 +1,20 @@
+/**
+ * 底部版权信息
+ */
+export default {
+  friendLinks: [
+    {
+      label: "狐璃",
+      // icon: "/icon/user.svg",
+      href: "https://gitee.com/fox-glaze",
+    },
+    {
+      label: "Gitee",
+      href: "https://gitee.com/fox-glaze",
+    },
+  ],
+  copyright: {
+    href: "zhilaohu.icu",
+    name: "HL",
+  },
+};
